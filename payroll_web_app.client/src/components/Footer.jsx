92f24e313f57d29ad473a867744dec515409d8e0
klaumail109 <klaumail109@gmail.com>
2025-10-15 14:04:16 -0700

    Add Footer component with copyright notice

---
+++ payroll_web_app.client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// src/components/Footer.jsx
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="shared-footer">
+      <p>© 2025 Payroll System. All rights reserved.</p>
+    </footer>
+  );
+}
