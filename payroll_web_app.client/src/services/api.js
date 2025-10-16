6f5e0904d1ae0f2a17c70282149412536bc1d2d2
Kamil Afzal <kamilafzal147@hotmail.com>
2025-10-15 20:03:16 -0700

    conect to correct api ports, but not tested

--- payroll_web_app.client/src/services/api.js
+++ payroll_web_app.client/src/services/api.js
@@ -1,13 +1,13 @@
 // services/api.js
-const API_BASE = "http://localhost:5000/api"; // Adjust if your backend runs on a different port
+const API_BASE = "https://localhost:56644/Users"; // Adjust if your backend runs on a different port
 
 export const getEmployees = async () => {
-  const response = await fetch(`${API_BASE}/employee`);
+    const response = await fetch(`${API_BASE}/getusers`);
   return response.json();
 };
 
 export const addEmployee = async (employeeData) => {
-  const response = await fetch(`${API_BASE}/employee`, {
+    const response = await fetch(`${API_BASE}/adduser`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(employeeData),
