59f9c746f0aa91905261f59f7116d68b9df7a3de
Kamil Afzal <kamilafzal147@hotmail.com>
2025-10-19 18:44:09 -0700

    Temp

--- Payroll_Web_App.Server/Program.cs
+++ Payroll_Web_App.Server/Program.cs
@@ -22,11 +22,12 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
