9081d911ae22dc3f1d80f26740940f19cdb8029f
antman160 <antman160@gmail.com>
2025-10-27 20:35:06 -0700

    defined our roles
    added our roles

---
+++ Payroll_Web_App.Server/Models/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace Payroll_Web_App.Server.Models
+{
+    public class Roles
+    {
+        public const string Admin = "Admin";
+
+        public const string HR = "HR";
+
+        public const string Finance = "Finance";
+
+        public const string Employee = "Employee";
+    }
+}
