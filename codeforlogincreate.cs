46f868eb7d51d386cc41cece4cc8be9ac7c9875f
thinkrc2 <thinkrc2@gmail.com>
2025-11-17 00:13:29 -0800

    Add files via upload

---
+++ codeforlogincreate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//FOR CREATING USERS
+using Payroll.Hashing;
+
+// For registration:
+ var stored = PasswordHasher.HashPassword(plainTextPassword);
+// Save `stored` in your Users table 
+
+
+//FOR LOGGING IN USERS
+// Retrieve the user's stored hash string from DB
+var stored = user.PasswordHash;
+
+bool ok = PasswordHasher.Verify(enteredPassword, stored);
+if (!ok)
+{
+    // invalid credentials
+}
+else
+{
+    // login success
+    // Optionally upgrade the hash if parameters changed:
+    if (PasswordHasher.NeedsRehash(stored))
+    {
+        user.PasswordHash = PasswordHasher.HashPassword(enteredPassword);
+        // save changes to Database
+    }
+}
+
